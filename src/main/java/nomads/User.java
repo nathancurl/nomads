877bc4b06e5ed0a046334203636b0a37cbebf546
Mustafa Sameen <s_sameen@coloradocollege.edu>
2023-03-21 09:37:52 -0600

    Update User.java

--- src/main/java/nomads/User.java
+++ src/main/java/nomads/User.java
@@ -127,44 +127,6 @@ public class User {
         this.food = food;
     }
 
-    public void updateRetuningUser(String path) {
-        File file = new File("users", path);
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        String line = scanner.nextLine();
-        line = scanner.nextLine();
-        String[] arr = line.split(" ");
-        this.nationality = arr[1];
-        if (scanner.hasNextLine()) {
-            line = scanner.nextLine().substring(12);
-            arr = line.split(", ");
-            String lastWord = arr[arr.length-1];
-            lastWord = lastWord.substring(0, lastWord.length()-1);
-            arr[arr.length-1] = lastWord;
-            this.favorites = new ArrayList<String>(Arrays.asList(arr));
-        } else {
-            this.favorites = new ArrayList<>();
-        }
-
-    }
-
-
-    public void close() throws FileNotFoundException {
-        File file = new File("users", username);
-        PrintWriter writer = new PrintWriter(file);
-        try {
-            writer = new PrintWriter(file);
-            writer.write("Username: " + (username) + "\n" + "Nationality: " + (nationality) + "\n" + "Favorites: " + favorites);
-            writer.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
     public void setFavorites(String faves) {
         String[] words = faves.split(", ");
